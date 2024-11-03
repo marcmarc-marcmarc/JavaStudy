e3ef6325000d55d29c9598868c8fe2118eeb4c95
Marc <49604378+marcmarc-marcmarc@users.noreply.github.com>
2024-11-03 22:53:03 +0000

    saving

--- test.java
+++ test.java
@@ -1,5 +1,8 @@
 public class test{
-  public static void main (string[] args) {
-    System.out.println(x: "Hello world")
+
+  public static void main (String[] args) {
+    System.out.println("Hello world");
+ 
+    System.out.println("x?");
   }
 }
