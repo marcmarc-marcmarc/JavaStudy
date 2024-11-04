512528e6e2905af356cfa1be875854154f94c2c3
Marc <49604378+marcmarc-marcmarc@users.noreply.github.com>
2024-11-04 17:26:02 +0000

    save

--- moocpart1.java
+++ moocpart1.java
@@ -1,10 +1,13 @@
-public class moocpart1 {
-  public static void main(String[]args) {
+import java.util.Scanner;
+public class moocpart1{
 
-    System.out.println("Once upon a time");
-    System.out.println("there was");
-    System.out.println("a program");
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("write a message:");
 
+    String message = scanner.nextLine();
+    
+    System.out.println("Your message was:  " + message);
 
   }
 }
